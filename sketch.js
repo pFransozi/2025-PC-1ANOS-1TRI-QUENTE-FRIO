3036c8c419300a00a642df0af4eee4b1b865eb1f
Philipe Fransozi <philipehf@gmail.com>
2025-02-09 20:15:34 -0300

    add aula01

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let x;
+let y;
+
 function setup() {
-  //createCanvas(width, height)
   createCanvas(400, 400);
+  x = random(400);
+  x = int(x);
+  y = random(400);
+  y = int(y);
 }
 
 function draw() {
+  background(220);
+  circle(x, y, 10);
+  console.log(mouseX,x);
+
+  if (mouseX == x ) {
+    text("Encontrei!", 200, 200);
+    noLoop();
+  }
 }
